44c56489da8ad73fdd53868a30f615be719c28e9
ziyech18 <jassimalikk8@gmail.com>
2024-12-23 19:25:27 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "CRACKER";
-    string public symbol = "CRACKED";
+    string public name = "jasimcoin";
+    string public symbol = "jasi";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
